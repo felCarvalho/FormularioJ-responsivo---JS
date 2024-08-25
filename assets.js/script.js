5d932b642c01c0aa8ee1e434453f51f71b48c939
felipe carvalho <felcarvalho54321@gmail.com>
2024-08-24 22:04:44 -0300

    eliminando partes inuteis de codigo e refinando a verificação da condição de string vazia nos input

--- assets.js/script.js
+++ assets.js/script.js
@@ -1,24 +1,37 @@
 "use strict";
+
+//capturando valores de mensagens, pop-up, e etc.
 const formulario = document.querySelector(".form");
 const PoupUp = document.querySelector(".container-mensagem-error");
 const fundoBlur = document.querySelector(".fundo-blur");
 const close = document.querySelector("#button-close");
 const mensagemPouUp = document.querySelector(".mensagem-error");
 
+//evento de click acionando o formulario e usando preventDefault para cortar a ligação do form com banco de dados.
 formulario.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  //selecionando todos inputs
   const inputsValue = document.querySelectorAll("input");
+  //aplicando valor true a todos os inputs.
+  let verificar = true;
 
+  //loop for percorrendo todos os inputs dentro da NodeList.
   for (let i = 0; i < inputsValue.length; i++) {
-    const valor = inputsValue[i].value;
-    console.log(valor);
+    //guardando todos os valores dos input do html na variavel valor e usando trim(para ver se á espaços extra nos valores)
+    const valor = inputsValue[i].value.trim();
 
-    if (valor.trim() === "") {
-      mensagemPouUp.textContent = `Preencha todos os campos!`;
+    //verificando se existe algum campo totalmente sem valor no form e exxibindo caso seja encontrado.
+    if (valor === "") {
+      verificar = false;
+      return;
+    }
+
+    if (!verificar === "") {
+      mensagemPouUp.textContent = `Formulario enviado com sucesso!`;
       PoupUpAtivado();
     } else {
-      mensagemPouUp.textContent = `Formulario enviado com sucesso!`;
+      mensagemPouUp.textContent = `Preencha todos os campos!`;
       PoupUpAtivado();
     }
   }
